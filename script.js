659c62ec5c3b287849d9230e5afb0e2e53829873
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-18 13:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let name = document.getElementById("name");
+let pass = document.getElementById("pass");
+let num = document.getElementById("num");
+let check = document.getElementById("check");
+let btn = document.getElementById("btn");
+let form = document.getElementById("form");
+
+check.addEventListener("click", () =>{
+  if (check.checked) {
+    btn.disabled = false
+  } else {
+    btn.disabled = true
+  }
+})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+	if(name.value != "" && pass.value != "" && num.value != ""){
+		form.submit();
+	}
+});
